ce703f349a8ea2fab36e3dae7ba47c24cf207e9d
Adnan <amaru009@fiu.edu>
2021-11-10 14:58:29 -0500

    Added another ycsb script

---
+++ scripts/run_ycsb_workloads_2.sh
@@ -0,0 +1,14 @@
+!/bin/sh
+ycsb-0.15.0/bin/ycsb load memcached -s -P ycsb-0.15.0/workloads/workloada -p "memcached.hosts=127.0.0.1" -p "memcached.port=11211" -p recordcount=1000000000 -threads 64 >> outRun.ycsb.load
+sleep 1m
+ycsb-0.15.0/bin/ycsb run memcached -s -P ycsb-0.15.0/workloads/workloada -p "memcached.hosts=127.0.0.1" -p "memcached.port=11211" -p recordcount=1000000000 -p operationcount=666666667 -threads 64 >> outRun.ycsb.a
+sleep 1m
+ycsb-0.15.0/bin/ycsb run memcached -s -P ycsb-0.15.0/workloads/workloadb -p "memcached.hosts=127.0.0.1" -p "memcached.port=11211" -p recordcount=1000000000 -p operationcount=666666667 -threads 64 >> outRun.ycsb.b
+sleep 1m
+ycsb-0.15.0/bin/ycsb run memcached -s -P ycsb-0.15.0/workloads/workloadc -p "memcached.hosts=127.0.0.1" -p "memcached.port=11211" -p recordcount=1000000000 -p operationcount=666666667 -threads 64 >> outRun.ycsb.c
+sleep 1m
+ycsb-0.15.0/bin/ycsb run memcached -s -P ycsb-0.15.0/workloads/workloadf -p "memcached.hosts=127.0.0.1" -p "memcached.port=11211" -p recordcount=1000000000 -p operationcount=666666667 -threads 64 >> outRun.ycsb.f
+sleep 1m
+ycsb-0.15.0/bin/ycsb run memcached -s -P ycsb-0.15.0/workloads/workloadw -p "memcached.hosts=127.0.0.1" -p "memcached.port=11211" -p recordcount=1000000000 -p operationcount=666666667 -threads 64 >> outRun.ycsb.w
+sleep 1m
+ycsb-0.15.0/bin/ycsb run memcached -s -P ycsb-0.15.0/workloads/workloadd -p "memcached.hosts=127.0.0.1" -p "memcached.port=11211" -p recordcount=1000000000 -p operationcount=666666667 -threads 64 >> outRun.ycsb.d
