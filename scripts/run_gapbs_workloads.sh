9f18aaea264c3e44ae9dfad306f8d725eb797aec
Adnan Maruf <44008897+adnanmaruf@users.noreply.github.com>
2021-11-10 17:49:44 -0500

    Update run_gapbs_workloads.sh

--- scripts/run_gapbs_workloads.sh
+++ scripts/run_gapbs_workloads.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-sudo gapbs/bfs -g 30 -n 5 > bfs.out
+gapbs/bfs -g 30 -n 5 > bfs.out
 sleep 100
-sudo gapbs/pr -g 30 -n 5 > pr.out
+gapbs/pr -g 30 -n 5 > pr.out
 sleep 100
-sudo gapbs/sssp -g 29 -n 5 > sssp.out
+gapbs/sssp -g 29 -n 5 > sssp.out
 sleep 100
-sudo gapbs/tc -g 30 -n 5 > tc.out
+gapbs/tc -g 30 -n 5 > tc.out
 sleep 100
-sudo gapbs/bc -g 30 -n 5 > bc.out
+gapbs/bc -g 30 -n 5 > bc.out
 sleep 100
-sudo gapbs/cc -g 30 -n 5 > cc.out
+gapbs/cc -g 30 -n 5 > cc.out
 sleep 100
 
 
